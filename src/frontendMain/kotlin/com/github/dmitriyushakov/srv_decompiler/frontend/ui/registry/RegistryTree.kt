c6b0a8a87f390d63761958cca22b7e0d60089794
Dmitriy Ushakov <dmitryushakov1@yandex.ru>
2023-10-01 00:20:54 +0400

    Show registry index tree on frontend.

---
+++ src/frontendMain/kotlin/com/github/dmitriyushakov/srv_decompiler/frontend/ui/registry/RegistryTree.kt
@@ -0,0 +1,77 @@
+package com.github.dmitriyushakov.srv_decompiler.frontend.ui.registry
+
+import com.github.dmitriyushakov.srv_decompiler.frontend.api.API
+import com.github.dmitriyushakov.srv_decompiler.frontend.component.common.reactFontIcon
+import com.github.dmitriyushakov.srv_decompiler.frontend.component.tree.Tree
+import com.github.dmitriyushakov.srv_decompiler.frontend.component.tree.TreeNode
+import com.github.dmitriyushakov.srv_decompiler.frontend.model.ItemType
+import com.github.dmitriyushakov.srv_decompiler.frontend.utils.runPromise
+import io.kvision.core.Container
+import io.kvision.panel.SimplePanel
+import io.kvision.react.reactBind
+import io.kvision.state.ObservableValue
+import react.ChildrenBuilder
+
+private fun ItemType.toFAIconClasses(): String = when(this) {
+    ItemType.Package -> "fa-solid fa-box"
+    ItemType.Class -> "fa-solid fa-file"
+    ItemType.Field -> "fa-solid fa-hashtag"
+    ItemType.Method -> "fa-solid fa-bolt"
+    ItemType.LocalVar -> "fa-solid fa-hashtag"
+}
+
+class RegistryTree: SimplePanel("registry-tree") {
+    val treeData: ObservableValue<List<RegistryTreeNode>> = ObservableValue(listOf())
+
+    private fun ChildrenBuilder.renderTreeNodes(nodesData: List<RegistryTreeNode>) {
+        for (node in nodesData) {
+            TreeNode {
+                key = node.key
+                title = node.title
+                isLeaf = node.isLeaf
+                icon = reactFontIcon(node.itemType.toFAIconClasses())
+
+                val children = node.children
+                if (children != null) renderTreeNodes(children)
+            }
+        }
+    }
+
+    init {
+        reactBind(treeData) { accessTree, modifyTree ->
+            val nodesData = accessTree()
+
+            Tree {
+                loadData = { node ->
+                    runPromise {
+                        val nodesData = accessTree()
+                        val eventKey: String = node.props.eventKey
+                        val treeNode = nodesData.findByKey(eventKey)
+                        if (treeNode != null) {
+                            val response = API.IndexRegistry.listPackage(treeNode.path)
+
+                            val newChildren = response.toRegistryTreeNodes(treeNode.path)
+                            val newNodesData = nodesData.replaceChildrenAtKey(eventKey, newChildren)
+                            this@RegistryTree.treeData.setState(newNodesData)
+                        }
+                    }
+                }
+
+                renderTreeNodes(nodesData)
+            }
+        }
+
+        addAfterInsertHook {
+            runPromise {
+                val response = API.IndexRegistry.listPackage(listOf("."))
+                treeData.setState(response.toRegistryTreeNodes(emptyList()))
+            }
+        }
+    }
+}
+
+fun Container.registryTree(): RegistryTree {
+    val tree = RegistryTree()
+    add(tree)
+    return tree
+}
---
+++ src/frontendMain/kotlin/com/github/dmitriyushakov/srv_decompiler/frontend/ui/registry/RegistryTreeNode.kt
@@ -0,0 +1,88 @@
+package com.github.dmitriyushakov.srv_decompiler.frontend.ui.registry
+
+import com.github.dmitriyushakov.srv_decompiler.frontend.model.ItemType
+import com.github.dmitriyushakov.srv_decompiler.frontend.model.ListPackageResponse
+import com.github.dmitriyushakov.srv_decompiler.frontend.model.Path
+import com.github.dmitriyushakov.srv_decompiler.frontend.utils.pathToString
+
+class RegistryTreeNode(
+    val key: String,
+    val title: String,
+    val itemType: ItemType,
+    val isLeaf: Boolean,
+    val path: Path,
+    val children: List<RegistryTreeNode>? = null
+) {
+    fun findByKey(key: String): RegistryTreeNode? {
+        if (key == this.key) {
+            return this
+        } else if (children != null) {
+            for (node in children) {
+                val foundNode = node.findByKey(key)
+                if (foundNode != null) return foundNode
+            }
+        }
+        return null
+    }
+
+    fun replaceChildren(children: List<RegistryTreeNode>?): RegistryTreeNode {
+        return RegistryTreeNode(
+            key = this.key,
+            title = this.title,
+            itemType = this.itemType,
+            isLeaf = this.isLeaf,
+            path = this.path,
+            children = children
+        )
+    }
+
+    private fun transformAtKeyOrNull(key: String, transformation: (RegistryTreeNode) -> RegistryTreeNode): RegistryTreeNode? {
+        if (key == this.key) {
+            return transformation(this)
+        } else if (children != null) {
+            val newChildren: MutableList<RegistryTreeNode> = mutableListOf()
+            var anyModified = false
+
+            for (child in children) {
+                val newChild = child.transformAtKeyOrNull(key, transformation)
+                if (newChild == null) newChildren.add(child)
+                else {
+                    newChildren.add(newChild)
+                    anyModified = true
+                }
+            }
+
+            if (anyModified) {
+                return replaceChildren(newChildren)
+            } else return null
+        } else return null
+    }
+
+    fun transformAtKey(key: String, transformation: (RegistryTreeNode) -> RegistryTreeNode): RegistryTreeNode {
+        return transformAtKeyOrNull(key, transformation) ?: this
+    }
+
+    fun replaceChildrenAtKey(key: String, children: List<RegistryTreeNode>?): RegistryTreeNode =
+        transformAtKey(key) { it.replaceChildren(children) }
+}
+
+fun List<RegistryTreeNode>.transformAtKey(key: String, transformation: (RegistryTreeNode) -> RegistryTreeNode): List<RegistryTreeNode> =
+    map { it.transformAtKey(key, transformation) }
+
+fun List<RegistryTreeNode>.replaceChildrenAtKey(key: String, children: List<RegistryTreeNode>?): List<RegistryTreeNode> =
+    map { it.replaceChildrenAtKey(key, children) }
+
+fun List<RegistryTreeNode>.findByKey(key: String): RegistryTreeNode? = firstNotNullOfOrNull { it.findByKey(key) }
+
+fun ListPackageResponse.Item.toRegistryTreeNode(nodePathPrefix: Path): RegistryTreeNode {
+    val path: Path = nodePathPrefix + listOf(name)
+    return RegistryTreeNode(
+        key = pathToString(path),
+        title = name,
+        itemType = itemType,
+        isLeaf = !haveItemsInside,
+        path = path
+    )
+}
+
+fun ListPackageResponse.toRegistryTreeNodes(nodePathPrefix: Path): List<RegistryTreeNode> = items.map { it.toRegistryTreeNode(nodePathPrefix) }
